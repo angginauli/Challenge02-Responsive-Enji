3bc65e1c9c20b1b1da013cc0157fd81e44ca80bb
angginauli <86095799+angginauli@users.noreply.github.com>
2022-09-30 00:25:49 +0700

    Add files via upload

---
+++ Challenge-02 Enji/js/script.js
@@ -0,0 +1,24 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+        },
+        768: {
+            items: 2,
+        },
+        1100: {
+            items: 2,
+        },
+        1400: {
+            items: 2,
+        }
+    },
+    nav: true,
+    dots: false,
+    navText: [
+    ],
+});
+
+
